0f01b5087dffaed651be7c71a90b8cec3ea9a7ca
trex-coder <110154601+trex-coder@users.noreply.github.com>
2024-05-01 17:04:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import Head from 'next/head'; // Correct import statement for Head
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
-import { Button, Form, Grid, Header, Message, Radio, Segment } from 'semantic-ui-react';
+import { Button, Grid, Header, Message, Segment } from 'semantic-ui-react';
 
 export default function Home() {
-  const [dynamicUrl, setDynamicUrl] = useState("");
+  const [dynamicUrl, setDynamicUrl] = useState('');
   const [downloading, setDownloading] = useState(false);
-  const [err, setErr] = useState("");
+  const [err, setErr] = useState('');
 
   useEffect(() => {
-  const url = window.location.origin.replace('localhost', '127.0.0.1') +
-    '/api/getM3u?sid=' + 'tplay' +
-    '_A&id=' + '123456789' +
-    '&sname=' + 'tataP' +
-    '&tkn=' + 'xeotpxyastrplg';
-
-  setDynamicUrl(url);
-}, []);
+    const url = `${window.location.origin.replace('localhost', '127.0.0.1')}/api/getM3u?sid=tplay_A&id=123456789&sname=tataP&tkn=xeotpxyastrplg`;
+    setDynamicUrl(url);
+  }, []);
 
   function downloadM3uFile(filename) {
     setDownloading(true);
@@ -24,16 +19,14 @@ export default function Home() {
       redirect: 'follow'
     };
 
-    fetch(window.location.origin + '/api/getM3u?sid=' + 'tplay' + '_' + 'A' + '&id=' + '123456789' + '&sname=' + 'tataP' + '&tkn=' + 'xeotpxyastrplg', requestOptions)
+    fetch(`${window.location.origin}/api/getM3u?sid=tplay_A&id=123456789&sname=tataP&tkn=xeotpxyastrplg`, requestOptions)
       .then(response => response.text())
       .then(result => {
-        console.log(result);
         const data = result;
         const blob = new Blob([data], { type: 'text/plain' });
         if (window.navigator.msSaveOrOpenBlob) {
           window.navigator.msSaveBlob(blob, filename);
-        }
-        else {
+        } else {
           const elem = window.document.createElement('a');
           elem.href = window.URL.createObjectURL(blob);
           elem.download = filename;
@@ -44,7 +37,7 @@ export default function Home() {
         setDownloading(false);
       })
       .catch(error => {
-        console.log('error', error);
+        console.error('Error downloading file:', error);
         setDownloading(false);
       });
   }
@@ -58,55 +51,46 @@ export default function Home() {
           content="Easiest way to generate a Tata Play IPTV (m3u) playlist for the channels you have subscribed to."
         />
       </Head>
-      <Grid columns='equal' padded centered>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column computer={8} tablet={12} mobile={16}>
-            <Segment loading={downloading}>
-              <Header as={'h1'}>Generate Tata Play m3u</Header>
-              <Message>
-                <Message.Header>Dynamic URL to get m3u: </Message.Header>
-                <img src={`https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${encodeURIComponent(dynamicUrl)}&size=small`} alt="QR Code" />
-                <p>
-                  <a href={dynamicUrl}>{dynamicUrl}</a>
-                </p>
-                <p>
-                  You can use the above m3u URL in OTT Navigator or Tivimate app to watch all channels.
-                </p>
-                <p>
-                  Set reload data to 10 Min in provider setting of Ott Navigator player &  Enjoy!
-                </p>
-                <Message.Header>You cannot generate a permanent m3u file URL on localhost but you can download your m3u file: </Message.Header>
-                <p></p>
-                <p>
-                  <Button loading={downloading} primary onClick={() => downloadM3uFile('ts.m3u')}>Download m3u file</Button>
-                </p>
-                <p>The downloaded m3u file will be valid only for 10 Minutes.</p>
-              </Message>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-        <Grid.Row style={{ display: err === '' ? 'none' : 'block' }}>
-          <Grid.Column></Grid.Column>
-          <Grid.Column computer={8} tablet={12} mobile={16}>
-            <Message color='red'>
-              <Message.Header>Error</Message.Header>
+      <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 600 }}>
+          <Segment loading={downloading}>
+            <Header as="h1" textAlign="center">
+              Generate Tata Play m3u
+            </Header>
+            <Message>
+              <Message.Header>Dynamic URL to get m3u:</Message.Header>
+              <img src={`https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${encodeURIComponent(dynamicUrl)}&size=small`} alt="QR Code" />
               <p>
-                {err}
+                <a href={dynamicUrl}>{dynamicUrl}</a>
               </p>
+              <p>
+                You can use the above m3u URL in OTT Navigator or Tivimate app to watch all channels.
+              </p>
+              <p>
+                Set reload data to 10 Min in provider setting of Ott Navigator player & Enjoy!
+              </p>
+              <Message.Header>You cannot generate a permanent m3u file URL on localhost but you can download your m3u file:</Message.Header>
+              <p>
+                <Button primary onClick={() => downloadM3uFile('ts.m3u')} loading={downloading}>
+                  Download m3u file
+                </Button>
+              </p>
+              <p>The downloaded m3u file will be valid only for 10 Minutes.</p>
             </Message>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-            <a href="https://github.com/lalitjoshi06/tataplay_url" target="_blank" rel="noreferrer">View source code on Github</a>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
+            {err && (
+              <Message negative>
+                <Message.Header>Error</Message.Header>
+                <p>{err}</p>
+              </Message>
+            )}
+            <p style={{ marginTop: '1rem' }}>
+              <a href="https://github.com/lalitjoshi06/tataplay_url" target="_blank" rel="noreferrer">
+                View source code on Github
+              </a>
+            </p>
+          </Segment>
+        </Grid.Column>
       </Grid>
     </div>
-  )
+  );
 }
