2ad6d034c5d86baa649efddbe4fddefba949c6c4
trex-coder <110154601+trex-coder@users.noreply.github.com>
2024-05-01 15:09:08 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -7,7 +7,7 @@ const getUserChanDetails = async () => {
     let obj = { list: [] };
 
     try {
-        const responseHmac = await fetch("https://raw.githubusercontent.com/trex-coder/hmac.json/main/hmac.json");
+        const responseHmac = await fetch("https://tplayapi.code-crafters.app/321codecrafters/hmac.json");
         const data = await responseHmac.json();
         hmacValue = data.data.hmac.hdnea.value;
     } catch (error) {
