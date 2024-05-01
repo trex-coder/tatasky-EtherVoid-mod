dc05500ba9d86e4eea8e4669ae4e61a525cf438d
trex-coder <110154601+trex-coder@users.noreply.github.com>
2024-05-01 08:47:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tata Sky m3u generator web app
+This project is totally based on CodeCrafters API.
+
+A react web app to generate an m3u playlist for the channels Available on Tata Sky.
+
+## Update
+Converted to a single repo to make it a one click deploy on vercel as a lot of you non-technical people were facing difficulties while deploying.
+## Deploy on vercel and access from anywhere (Easy)
+1) Click the button below to deploy.<br>
+<a href="https://vercel.com/new/clone?repository-url=https://github.com/trex-coder/Tp_gaurav"><img src="https://vercel.com/button" alt="Deploy"/></a><br>
+You will need an account on Vercel. It's free!
+
+<a href="https://render.com/deploy?repo=https://github.com/trex-coder/Tp_gaurav"><img src="https://render.com/images/deploy-to-render-button.svg" alt="Deploy"/></a><br>
+You will need an account on Render. It's free!
+Enjoy!
+## Run locally (You need a little bit of git and npm knowledge)
+1) Clone this repo.
+2) Open cmd / terminal and type the below commands one by one.<br>
+a) npm install<br>
+b) npm run dev
+3) Enjoy!
+
+## Note : **use this playlist on OTT Navigator and set to reload data to 10 min bcz hmac expires in every 10 min for most of channels-modified.Modified by trex
+        
+Special thanks to CodeCrafters for their API.
+
---
+++ pages/api/getM3u.js
@@ -0,0 +1,85 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import fetch from "cross-fetch";
+
+const getUserChanDetails = async () => {
+    let hmacValue;
+    let obj = { list: [] };
+
+    try {
+        const responseHmac = await fetch("https://raw.githubusercontent.com/trex-coder/hmac.json/main/hmac.json");
+        const data = await responseHmac.json();
+        hmacValue = data.data.hmac.hdnea.value;
+    } catch (error) {
+        console.error('Error fetching and rearranging HMAC data:', error);
+        return obj;
+    }
+
+    try {
+        const responseChannels = await fetch("https://tplayapi.code-crafters.app/321codecrafters/fetcher.json");
+        const cData = await responseChannels.json();
+
+        if (cData && cData.data && Array.isArray(cData.data.channels)) {
+            const flatChannels = cData.data.channels.flat();
+            flatChannels.forEach(channel => {
+                let firstGenre = channel.genres && channel.genres.length > 0 ? channel.genres[0] : null;
+                let rearrangedChannel = {
+                    id: channel.id,
+                    name: channel.name,
+                    tvg_id: channel.tvg_id,
+                    group_title: firstGenre,
+                    tvg_logo: channel.logo_url,
+                    stream_url: channel.manifest_url,
+                    license_url: channel.license_url,
+                    stream_headers: channel.manifest_headers ? (channel.manifest_headers['User-Agent'] || JSON.stringify(channel.manifest_headers)) : null,
+                    drm: channel.drm,
+                    is_mpd: channel.is_mpd,
+                    kid_in_mpd: channel.kid_in_mpd,
+                    hmac_required: channel.hmac_required,
+                    key_extracted: channel.key_extracted,
+                    pssh: channel.pssh,
+                    clearkey: channel.clearkeys ? JSON.stringify(channel.clearkeys[0].base64) : null,
+                    hma: hmacValue
+                };
+                obj.list.push(rearrangedChannel);
+            });
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+        return obj;
+    }
+
+    return obj;
+};
+
+const generateM3u = async (ud) => {
+    let m3uStr = '';
+
+    let userChanDetails = await getUserChanDetails();
+    let chansList = userChanDetails.list;
+
+    m3uStr = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mitthu786/tvepg/main/tataplay/epg.xml.gz"\n\n';
+
+    for (let i = 0; i < chansList.length; i++) {
+        m3uStr += '#EXTINF:-1 tvg-id="' + chansList[i].id.toString() + '" ';
+        m3uStr += 'group-title="' + (chansList[i].group_title) + '", tvg-logo="https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/' + (chansList[i].tvg_logo) + '", ' + chansList[i].name + '\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
+        m3uStr += '#EXTVLCOPT:http-user-agent=' + chansList[i].stream_headers + '\n';
+        m3uStr += chansList[i].stream_url + '?' + chansList[i].hma + '\n\n';
+    }
+
+    console.log('all done!');
+    return m3uStr;
+};
+
+export default async function handler(req, res) {
+    let uData = {
+        tsActive: true
+    };
+
+    if (uData.tsActive) {
+        let m3uString = await generateM3u(uData);
+        res.status(200).send(m3uString);
+    }
+}
